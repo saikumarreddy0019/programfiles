bb344517b86b0d23fcb8ae1c165f33e353a5be60
unknown <you@example.com>
2024-07-15 11:02:16 +0530

    sai files

---
+++ jaggu.py
@@ -0,0 +1,9 @@
+a= 600
+b  = 700
+c = 1500
+d = 1700
+if a>b:
+    print("a is bigger number")
+ else:
+        print("b is bigger number")
+        
---
+++ pandu.java
@@ -0,0 +1,8 @@
+public class sai{
+    public static void main(String[] args)
+    int a = 20;
+    int b = 30;
+    int c = 40;
+    System .out .println(a+b)
+    System.out.println(a-b)
+}
